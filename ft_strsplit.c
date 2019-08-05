e04c4dc814ea811d75d4c7bd7c253718d690b2c6
Brock Pace <bpace@e1z1r7p23.42.us.org>
2019-08-04 22:18:05 -0700

    Final

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: bpace <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/28 03:21:57 by bpace             #+#    #+#             */
-/*   Updated: 2019/08/03 03:22:50 by bpace            ###   ########.fr       */
+/*   Updated: 2019/08/04 22:15:01 by bpace            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ static int	getmallen(char const *s, char c)
 		if (s[i] != c)
 		{
 			mallen++;
-			while (s[++i + 1] != c && s[i + 1] != '\0')
-				;
+			while (s[i + 1] != c && s[i + 1] != '\0')
+				i++;
 		}
 	}
 	return (mallen);
@@ -70,7 +70,7 @@ char		**ft_strsplit(char const *s, char c)
 		ansi2 = 0;
 		i = stupidnormline(s, i, c);
 		ans[ansi] = malloc(sizeof(char) * (getmallensmall(s + i, c) + 1));
-		while (s[i] && s[i] != c)
+		while (s[i] != c)
 			ans[ansi][ansi2++] = s[i++];
 		ans[ansi][ansi2] = '\0';
 	}
